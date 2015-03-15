07aa0ae968960e85648446d022dfcdb4a914a2e5
klecavoj <klecanda@email.cz>
2015-03-15 22:22:05 +0100

    despagetizace kodu

--- geom.py
+++ geom.py
@@ -1,4 +1,4 @@
-    
+
 def Update_geom(points):
     strs = []
     for p in points:
@@ -7,14 +7,11 @@ def Update_geom(points):
     return ','.join(strs)
 
 def AddGeometryL(schema, tab, cur):
-    q = "select AddGeometryColumn('%s','%s','geom%s',2065,'LINESTRING',2);" % (schema, tab, tab)
-    cur.execute(q)
-
     cur.execute('select id from %s' % tab)
     tab_ids=cur.fetchall()
 
     for i in tab_ids:
-        sql="""select souradnice_y,souradnice_x from sobr where id in 
+        sql="""select souradnice_y,souradnice_x from sobr where id in
         (select bp_id from sbp where %s_id=%s)""" % (tab, i[0])
         cur.execute(sql)
         rows=cur.fetchall()
@@ -30,15 +27,11 @@ def AddGeometryL(schema, tab, cur):
             print str(e)
 
 def AddGeometryP(schema,tab,tab_L,cur):
-    cur.execute('CREATE TABLE IF NOT EXISTS %s (id VARCHAR(30));' % tab)
-    q = "select AddGeometryColumn('%s','%s','geom%s',2065,'POLYGON',2);" % (schema, tab, tab)
-    cur.execute(q)
-
     cur.execute('select id from %s' % tab)
     tab_ids=cur.fetchall()
-    for i in tab_ids:
-        createGeometry(tab, tab_L, i, cur)
-        
+    for p in tab_ids:
+        createGeometry(tab, tab_L, p[0], cur)
+
     # get par_ids in tab_L
     if tab=='par':
         cur.execute('select DISTINCT par_id_1 from %s' % tab_L)
@@ -51,8 +44,55 @@ def AddGeometryP(schema,tab,tab_L,cur):
         cur.execute('select DISTINCT bud_id from %s' % tab_L)
         for p in cur.fetchall():
             createGeometry(tab, tab_L, p[0], cur)
-        
+
+def _hranaObsahujiciBod(idHran, bod, tabulkaHran, cur):
+    for idx, h in enumerate(idHran): # najde tu, obsahujici currpoint
+        sql="select ST_Distance((select geom%s from %s where id=%s),'%s')" % (tabulkaHran, tabulkaHran, h[0], bod)
+        cur.execute(sql)
+        vzdalenost = cur.fetchone()[0]
+        if vzdalenost == 0:
+            return idx
+    return None
+
+def _PGPoint2Floats(point, cur):
+    cur.execute("select ST_X('%s')" % point)
+    x = cur.fetchone()[0]
+    cur.execute("select ST_Y('%s')" % point)
+    y = cur.fetchone()[0]
+    return (x, y)
+
+def _hranyParcely(hp_ids, tab, tab_L, cur):
+    hp_points = []
+    for idHr in hp_ids: # naplni hp_points body
+        cur.execute("select ST_PointN(geom%s,1) from %s where id=%s" % (tab_L, tab_L, idHr[0]))
+        p1=cur.fetchone()[0]
+        cur.execute("select ST_PointN(geom%s,2) from %s where id=%s" % (tab_L, tab_L, idHr[0]))
+        p2=cur.fetchone()[0]
+        hp_points.append([p1,p2])
+
+    return hp_points
+
+def _2PolygonQuery(polydata):
+    return "ST_GeomFromText('POLYGON((%s))',2065)" % Update_geom(polydata)
+
+def _odNejvnejsnejsihoPoNejvnitrnejsi(PolyCoords, cur):
+    first=0
+    for ii in range(len(PolyCoords)):
+        for jj in range(ii+1,len(PolyCoords)):
+            cur.execute("select ST_Contains(%s,%s)" % \
+                (_2PolygonQuery(PolyCoords[ii]), _2PolygonQuery(PolyCoords[jj])))
+            if cur.fetchone()[0]==1:
+                first=1
+                pom=PolyCoords[0]
+                PolyCoords[0]=PolyCoords[ii]
+                PolyCoords[ii]=pom
+                break
+        if first==1:
+            break
+
 def createGeometry(tab, tab_L, par_id, cur):
+    if par_id == None: return
+
     cur.execute("SELECT id, geom%s FROM %s WHERE id='%s'" % (tab, tab, par_id))
     existing = cur.fetchone()
     if not existing:
@@ -60,21 +100,15 @@ def createGeometry(tab, tab_L, par_id, cur):
     elif existing[1]:
         # uz mame geometrii vyplnenou ...
         return
-            
-    hp_points=[]
+
     if tab=='par':
         sql="select id from %s where (par_id_1=%s) or (par_id_2=%s);" % (tab_L, par_id, par_id)
     elif tab=='bud':
-        sql="select id from %s where (bud_id=%s) and (obrbud_type='ob');" % (tab_L, par_id)
-        
+        sql="select id from %s where (bud_id=%s) and (obrbud_type='OB');" % (tab_L, par_id)
     cur.execute(sql)
-    hp_ids=cur.fetchall() #seznam hp k dane parcele
-    for j in hp_ids: #naplni hp_points body
-        cur.execute("select ST_PointN(geom%s,1) from %s where id=%s" % (tab_L, tab_L, j[0]))
-        p1=cur.fetchone()[0]
-        cur.execute("select ST_PointN(geom%s,2) from %s where id=%s" % (tab_L, tab_L, j[0]))
-        p2=cur.fetchone()[0]
-        hp_points.append([p1,p2])
+    hp_ids = cur.fetchall()  #seznam id hran k dane parcele
+
+    hp_points = _hranyParcely(hp_ids, tab, tab_L, cur)  # nacte hranice parcely
 
     PolyCoords=[]
 
@@ -84,80 +118,54 @@ def createGeometry(tab, tab_L, par_id, cur):
             break
         sp=sp+1
         PolyCoords.append([])
-        # print hp_points[0][0]
-        cur.execute("select ST_X('"+hp_points[0][0]+"')")
-        x=cur.fetchone()[0]
-        cur.execute("select ST_Y('"+hp_points[0][0]+"')")
-        y=cur.fetchone()[0]
+        x, y = _PGPoint2Floats(hp_points[0][0], cur)
         PolyCoords[sp].append([-x,-y])
-        point=hp_points[0][0]
-        hp_ids.pop(0)
-        hp_points.pop(0)
+        currpoint = hp_points[0][0]
+        hp_ids.pop(0)       # popne hranu
+        hp_points.pop(0)    # vezme 1. bod a popne ho ze seznamu
 
-        dalsi = 0
-        for k in range(len(hp_ids)):
-            for ind,h in enumerate(hp_ids): #najde hp, obsahujici bod point
-                sql="select ST_Distance((select geom%s from %s where id=%s),'%s')" % (tab_L, tab_L, h[0], point)
-                cur.execute(sql)
-                if cur.fetchone()[0]==0:
-                    dalsi=1
-                    break
-            if dalsi==0:
-                break
-            dalsi=0
-            for foo in range(2):
-                cur.execute("select ST_X('"+hp_points[ind][foo]+"')")
-                x=cur.fetchone()[0]
-                cur.execute("select ST_Y('"+hp_points[ind][foo]+"')")
-                y=cur.fetchone()[0]
+        for _ in range(len(hp_ids)):    # pro vsechny hrany
+            ind = _hranaObsahujiciBod(hp_ids, currpoint, tab_L, cur)
+            if ind == None: break
+
+            for foo in range(2): # pro oba body nalezene hrany
+                x, y = _PGPoint2Floats(hp_points[ind][foo], cur)
                 try:
-                    PolyCoords[sp].index([-x,-y])
-                except:
-                    PolyCoords[sp].append([-x,-y])
-                    point=hp_points[ind][foo]
+                    PolyCoords[sp].index([-x,-y])   # zkus jestli tam uz je
+                except ValueError:                  # kdyz ne
+                    PolyCoords[sp].append([-x,-y])  # prida ho do polygonu
+                    currpoint=hp_points[ind][foo]   # posune currpoint
                     hp_ids.pop(ind)
                     hp_points.pop(ind)
                     break
-        PolyCoords[sp].append(PolyCoords[sp][0])
+        PolyCoords[sp].append(PolyCoords[sp][0])    # uzavre polygon (1.bod)
 
-    poly=[]
+    # odstranime neuzavrene polygony (jak muzou vzniknout? Chyba v datech?
+    checked = []
     for p in PolyCoords:
-        if(len(p)) < 4:
-            continue    # neuzavrena cara
-        sqlt="select ST_GeomFromText('POLYGON((%s))',2065)" % Update_geom(p)
-        try:
-            cur.execute(sqlt)
-            poly.append(cur.fetchone()[0])
-        except Exception, e:
-            print str(e)
-#     if len(poly)>1:
-#         first=0
-#         for ii in range(len(poly)):
-#             for jj in range(ii+1,len(poly)):
-#                 cur.execute("select ST_Contains('"+poly[ii]+"','"+poly[jj]+"')")
-#                 if cur.fetchone()[0]==1:
-#                     first=1
-#                     pom=PolyCoords[0]
-#                     PolyCoords[0]=PolyCoords[ii]
-#                     PolyCoords[ii]=pom
-#                     break
-#             if first==1:
-#                 break
-    
+        if(len(p)) > 3:
+            checked.append(p)
+    if len(checked) == 0:
+        return
+    PolyCoords = checked
+
+    # Prehazuje poradi od nejvnejsnejsiho po nejvnitrnejsi
+    if len(PolyCoords)>1:
+        _odNejvnejsnejsihoPoNejvnitrnejsi(PolyCoords, cur)
+
+    # udela polygon
     parts = []
     for pc in PolyCoords:
-        if(len(p)) < 4:
-            continue    # neuzavrena cara
         parts.append('(%s)' % Update_geom(pc))
-    if len(parts) == 0:
-        return
-    sqlt="UPDATE %s SET geom%s=ST_GeomFromText('POLYGON(%s)',2065) WHERE id='%s'" % (tab, tab, ','.join(parts), par_id)
-    
+
+    # a ulozi ho
+    sqlt="UPDATE %s SET geom%s=ST_GeomFromText('POLYGON(%s)',2065) WHERE id='%s'" % \
+        (tab, tab, ','.join(parts), par_id)
     try:
         cur.execute(sqlt)
     except Exception, e:
         print str(e)
-        
+
     cur.connection.commit()
 
 
