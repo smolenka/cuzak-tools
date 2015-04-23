0a93802ac75848ff8de3eae5c8e0f9596120aa56
vencax <vencax77@gmail.com>
2015-04-23 18:49:48 +0200

    added method

--- cuzak_tools/geometryzer/utils.py
+++ cuzak_tools/geometryzer/utils.py
@@ -8,7 +8,16 @@ def addGeometryColumn(geomtype, schema, tab, col, cur):
         cur.connection.commit()
     except psycopg2.ProgrammingError:   # kdy chyba, uz tam je
         cur.connection.rollback()   # tak vratime zmeny - rollback
-        
+
+def point2GeoJSON(p):
+    return '{"type":"Point","coordinates":[-%s,-%s]}' % p
+
+def lineSet2MultiLineStringGeoJSON(lineset):
+    s = []
+    for l in lineset:
+        l = '[[-%s,-%s],[-%s,-%s]]' % (l[0][0], l[0][1], l[1][0], l[1][1])
+        s.append(l)
+    return '{"type":"MultiLineString","coordinates": [%s]}' % ',\n'.join(s)
 
 def pointListString(points):
     strs = []
@@ -19,6 +28,6 @@ def pointListString(points):
 
 def lines2LineString(points):
     """
-    Z paru bodu v tuples udela Linestring 
+    Z paru bodu v tuples udela Linestring
     """
     return 'LINESTRING(%s)' % pointListString(points)
