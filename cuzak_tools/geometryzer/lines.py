cc008ac9f68ff8a8bf9848d5062a831a4bec0358
vencax <vencax77@gmail.com>
2015-05-19 10:31:43 +0200

    line solving, logging

--- cuzak_tools/geometryzer/lines.py
+++ cuzak_tools/geometryzer/lines.py
@@ -1,14 +1,16 @@
 
+import logging
 import utils
 
 def addLinesGeometry(schema, tab, cur):
     utils.addGeometryColumn('LINESTRING', schema, 'lines', 'geom', cur)
 
     cur.execute('select id from %s' % tab)
-
     for i in cur.fetchall():
-        _solveLine(i[0], tab, cur)
-        
+        lines = _getLine(i[0], tab, cur)
+        for l in lines:
+            addLine(l, {'zdroj': 0, 'zdrojid': i[0] }, cur)
+
 def addLine(points, attrs, cur):
     q = """INSERT INTO lines (geom, %s) VALUES (
         ST_GeomFromText('LINESTRING(%s)',2065), %s
@@ -21,40 +23,46 @@ def addLine(points, attrs, cur):
         cur.execute(query, attrs.values())
         return cur.fetchone()[0]
     except Exception, e:
-        print str(e)
-        
+        logging.exception(e)
+
+def linieParcely(pid, cur):
+    lines = []
+    # vyberu vsechny hrany, ktere maji par_id_1 nebo par_id_2 rovno parId
+    cur.execute("""
+    select id as idckalinii from hp where par_id_1 = %s OR par_id_2 = %s
+    """, (pid, pid))
+    for lineId in cur.fetchall():
+        lines.extend(_getLine(lineId[0], 'hp', cur))
+    # a udelam z nich GeoJSON multilinii
+    return utils.lineSet2MultiLineStringGeoJSON(lines)
+
 def _lineAlreadyExists(itemid, zdrojid, zdroj, cur):
     q = "SELECT COUNT(*) FROM lines WHERE zdrojid=%s AND zdroj=%s"
     cur.execute(q, (str(itemid), zdroj))
     contains = cur.fetchone()
     return contains[0] > 0
-        
-def _solveLine(itemid, tab, cur):
-    if _lineAlreadyExists(itemid, itemid, 0, cur):
-        return
+
+def _getLine(itemid, tab, cur):
     sql="""select souradnice_y,souradnice_x from sobr where id in
     (select bp_id from sbp where %s_id=%s)""" % (tab, itemid)
     cur.execute(sql)
     rows=cur.fetchall()
     if len(rows) == 0:
-        return
+        return ()
     if len(rows) > 2:
-        _solveMultiline(itemid, cur)
+        return _getMultiline(itemid, cur)
     else:
-        addLine(rows, {'zdroj': 0, 'zdrojid': itemid }, cur)
-    
-def _solveMultiline(itemid, cur):
-    print 'WARN: multiline: %s' % itemid
-    
-    q = '''SELECT souradnice_y, souradnice_x 
-    FROM sobr o INNER JOIN sbp s ON o.id=s.bp_id 
+        return [(rows[0], rows[1])]
+
+def _getMultiline(itemid, cur):
+    logging.warn('WARN: multiline: %s' % itemid)
+    lines = []
+    q = '''SELECT souradnice_y, souradnice_x
+    FROM sobr o INNER JOIN sbp s ON o.id=s.bp_id
     WHERE s.hp_id=%s ORDER BY poradove_cislo_bodu ASC
     '''
     cur.execute(q, (itemid, ))
     points = cur.fetchall()
     for idx in range(len(points)-1):
-        addLine([points[idx], points[idx+1]], {
-            'zdroj': 0, 'zdrojid': itemid
-        }, cur)
-        
+        lines.append((points[idx], points[idx+1]))
 
